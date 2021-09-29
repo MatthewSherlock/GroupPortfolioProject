638a28ae41ba2175babc27438ce893d596c558a8
Matthew Sherlock <ms5crt@bolton.ac.uk>
2021-09-29 11:27:59 +0100

    Added UE4 Project

---
+++ Source/PortfolioProject/PortfolioProjectGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PortfolioProjectGameMode.h"
+#include "PortfolioProjectHUD.h"
+#include "PortfolioProjectCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+APortfolioProjectGameMode::APortfolioProjectGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = APortfolioProjectHUD::StaticClass();
+}
